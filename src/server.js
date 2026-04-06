2ce337b1deaa468067d30d800693796cf090eeda
Tattianerl <tattiane85@hotmail.com>
2026-04-06 12:00:07 -0300

    feat: versão final da ByteShop pronta para deploy

--- src/server.js
+++ src/server.js
@@ -110,7 +110,7 @@ app.post("/cart/checkout", (req, res) => {
 });
 
 
-
-app.listen(3000, () => {
-  console.log("🚀 API rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 API rodando na porta ${PORT}`);
 });
